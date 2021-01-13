03b6cf32d72656241c3f8c6b525b531d124a8167
Olivier Notteghem <oliviern@uber.com>
2021-01-12 20:09:33 -0800

    Cleanup app

--- apps_presidio_helix_app/module1663/src/main/java/module1663packageKt0/Foo0.kt
+++
@@ -1,9 +0,0 @@
-package module1663packageKt0;
-
-annotation class Foo0Fancy
-@Foo0Fancy
-class Foo0 {
-
-  fun foo0(){
-  }
-}
--- apps_presidio_helix_app/module692/src/main/java/module692packageKt0/Foo0.kt
+++
@@ -1,9 +0,0 @@
-package module692packageKt0;
-
-annotation class Foo0Fancy
-@Foo0Fancy
-class Foo0 {
-
-  fun foo0(){
-  }
-}
