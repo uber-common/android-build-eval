0f4078b837224375183945680d502c9f20cc4c57
Olivier Notteghem <oliviern@uber.com>
2021-02-23 20:06:04 -0800

    Fix BUCK scenario to avoid missing okBuck files 2/2

--- mobile_app1/setup_buck.sh
+++ mobile_app1/setup_buck.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-echo "Generating BUCK project file"
+echo ">>> Generating BUCK project file"
 ./gradlew -Dokbuck.wrapper=true okbuck --no-configuration-cache
+
+echo ">>> Generating BUCK wrapper"
 ./gradlew -Dokbuck.wrapper=true buckWrapper --no-configuration-cache
 
-echo "Testing BUCK project"
-./buckw query rootModule
+echo ">>> Shutting down Gradle daemon"
+./gradlew --stop
+
+if [[ -n "${DEVPOD_ENVIRONMENT}" ]]; then
+  echo ">>> Waiting for buckw lock. Issue with devpod machine only."
+  sleep 60
+fi
+
+echo ">>> Prefetching external dependencies"
+SKIP_OKBUCK=0 ./buckw build .okbuck/ext/...
+
+
+
